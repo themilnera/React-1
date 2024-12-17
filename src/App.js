5748a3a557d0293503b3f562238b29dfc4eff33a
themilnera <amilner32@gmail.com>
2024-12-17 14:23:47 -0700

    Initial commit, set up react project, added Counter and styling.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+      <div><h1>Welcome</h1> <Counter /> </div>
+  );
+}
+
+export default App;
